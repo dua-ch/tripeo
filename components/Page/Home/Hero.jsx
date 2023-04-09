21e83307014f43cb6bd1dda46448bff827e25ee6
duach <ch.robika@gmail.com>
2023-04-09 19:57:57 +0500

    .

--- components/Page/Home/Hero.jsx
+++ components/Page/Home/Hero.jsx
@@ -189,7 +189,7 @@ const Hero = () => {
               src="/images/laptop.png"
               alt="laptop palverse"
               width={1000}
-              height={500}
+              height={600}
             />
           </div>
           <div className="bg-[#ECF7FD] text-black py-2 w-screen my-2">
